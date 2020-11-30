fe66d43b39a533d3a535e1b04afb4b3a4c43d338
nmhummel <natalie.hummel@gmail.com>
2020-11-29 19:10:06 -0600

    commit tree 1c8c39135538d9f138767f4524d017df2981c7a2

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Natalie."
+    end
+
+    get '/hometown' do
+        "My hometown is Montgomery, AL!"
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is I Would Do Anything For Love"
+    end
+    
 end
